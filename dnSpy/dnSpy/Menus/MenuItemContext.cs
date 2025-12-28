289b85b989b9c390a345fb4b2882879d9d6d52af
xyrar <brinsleyharvey7@gmail.com>
2025-12-28 21:40:25 +0000

    Fix formatting and improve state management

--- dnSpy/dnSpy/Menus/MenuItemContext.cs
+++ dnSpy/dnSpy/Menus/MenuItemContext.cs
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2014-2019 de4dot@gmail.com
 
-    This file is part of dnSpy
+    This file is part of dnSpy.
 
     dnSpy is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -10,13 +10,13 @@
 
     dnSpy is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with dnSpy.  If not, see <http://www.gnu.org/licenses/>.
+    along with dnSpy. If not, see <http://www.gnu.org/licenses/>.
 */
-
+// editing
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -29,37 +29,45 @@ namespace dnSpy.Menus {
 		public Guid MenuGuid { get; }
 		public GuidObject CreatorObject => guidObjects[0];
 		public IEnumerable<GuidObject> GuidObjects => guidObjects;
+
 		readonly List<GuidObject> guidObjects;
+		readonly Dictionary<object, object> state;
+
+		static readonly object disposedObject = new object();
 
 		public MenuItemContext(Guid menuGuid, bool openedFromKeyboard, GuidObject creatorObject, IEnumerable<GuidObject>? guidObjects) {
 			MenuGuid = menuGuid;
 			OpenedFromKeyboard = openedFromKeyboard;
+
 			this.guidObjects = new List<GuidObject>();
 			this.guidObjects.Add(creatorObject);
+
 			if (guidObjects is not null)
 				this.guidObjects.AddRange(guidObjects);
+
 			state = new Dictionary<object, object>();
 		}
 
 		public T? GetOrCreateState<T>(object key, Func<T> createState) where T : class {
 			Debug2.Assert(key is not null);
-			T? value;
+
 			if (state.TryGetValue(key, out var o)) {
-				value = o as T;
+				var value = o as T;
 				Debug2.Assert(o is null || value is not null);
 				return value;
 			}
-			value = createState();
-			state[key] = value;
-			return value;
+
+			var newValue = createState();
+			state[key] = newValue;
+			return newValue;
 		}
-		readonly Dictionary<object, object> state;
 
 		public T Find<T>() {
 			foreach (var o in GuidObjects) {
 				if (o.Object is T)
 					return (T)o.Object;
 			}
+
 			return default!;
 		}
 
@@ -68,16 +76,16 @@ namespace dnSpy.Menus {
 		public void Dispose() {
 			if (IsDisposed)
 				return;
+
 			IsDisposed = true;
 			OnDisposed?.Invoke(this, EventArgs.Empty);
 			OnDisposed = null;
 
 			// Clear everything. We don't want to hold on to objects that could've gotten disposed,
-			// eg. IDocumentViewer. Those instances could throw ObjectDisposedException
+			// e.g. IDocumentViewer. Those instances could throw ObjectDisposedException.
 			guidObjects.Clear();
 			guidObjects.Add(new GuidObject(Guid.Empty, disposedObject));
 			state.Clear();
 		}
-		static readonly object disposedObject = new object();
 	}
 }
